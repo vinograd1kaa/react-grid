984643e3b26dbd4745e2643b93a2c8813b233413
vinograd1kaa <supermaneee5@gmail.com>
2022-12-22 13:48:01 +0200

    added other files :/

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import MyCustomGrid from "./MyCustomGrid";
+
+const App = () => {
+  return (
+    <div>
+      <MyCustomGrid />
+      <MyCustomGrid />
+      <MyCustomGrid />
+    </div>
+  );
+};
+
+export default App;
